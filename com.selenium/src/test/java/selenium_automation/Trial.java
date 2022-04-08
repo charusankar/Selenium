7842082a976afd8af066a8e4cf71f34a862b8571
Charumathy Sankararaman <charusankar92@gmail.com>
2022-04-08 13:40:54 -0500

    Trial Class

---
+++ com.selenium/src/test/java/selenium_automation/Trial.java
@@ -0,0 +1,17 @@
+package selenium_automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Trial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+	}
+
+}
